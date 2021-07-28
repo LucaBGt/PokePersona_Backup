b033bcef770ef5ea2cce74739be80767f33968b4
LucaBGt <32401333+LucaBGt@users.noreply.github.com>
2021-07-28 15:50:14 +0200

    + Added Agent OW

--- src/data/object_events/object_event_graphics.h
+++ src/data/object_events/object_event_graphics.h
@@ -371,3 +371,7 @@ const u32 gObjectEventPic_Lugia[] = INCBIN_U32("graphics/object_events/pics/poke
 const u16 gObjectEventPal_Lugia[] = INCBIN_U16("graphics/object_events/palettes/lugia.gbapal");
 const u32 gObjectEventPic_HoOh[] = INCBIN_U32("graphics/object_events/pics/pokemon/ho_oh.4bpp");
 const u16 gObjectEventPal_HoOh[] = INCBIN_U16("graphics/object_events/palettes/ho_oh.gbapal");
+
+//New OWs
+const u32 gObjectEventPic_Agent[] = INCBIN_U32("graphics/object_events/pics/people/agent.4bpp");
+const u16 gObjectEventPal_Agent[] = INCBIN_U16("graphics/object_events/palettes/agent.gbapal");
--- src/data/object_events/object_event_graphics_info_pointers.h
+++ src/data/object_events/object_event_graphics_info_pointers.h
@@ -244,6 +244,8 @@ const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_Storyteller;
 const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_Giddy;
 const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_UnusedMauvilleOldMan1;
 const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_UnusedMauvilleOldMan2;
+//New OWs
+const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_Agent;
 
 
 const struct ObjectEventGraphicsInfo *const gObjectEventGraphicsInfoPointers[NUM_OBJ_EVENT_GFX] = {
@@ -486,6 +488,9 @@ const struct ObjectEventGraphicsInfo *const gObjectEventGraphicsInfoPointers[NUM
     [OBJ_EVENT_GFX_LINK_RS_MAY] =              &gObjectEventGraphicsInfo_RubySapphireMay,
     [OBJ_EVENT_GFX_LUGIA] =                    &gObjectEventGraphicsInfo_Lugia,
     [OBJ_EVENT_GFX_HOOH] =                     &gObjectEventGraphicsInfo_HoOh,
+    //New OWs
+    [OBJ_EVENT_GFX_AGENT] =                     &gObjectEventGraphicsInfo_Agent,
+
 };
 
 const struct ObjectEventGraphicsInfo *const gMauvilleOldManGraphicsInfoPointers[] = {
